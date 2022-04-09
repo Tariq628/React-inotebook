a3ea864fa495cf6baf992206ca81b4a5f0317507
tariq628 <tariqkaleri628@gmail.com>
2022-04-09 07:12:50 +0500

    Hash password method and jwt added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const connectToMongo = require('./db');
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 5000;
 app.use(express.json());
 connectToMongo();
 
-// available routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// available routes;
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const JWT_SECRET = "harryisagoodb$oy";
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const {body, validationResult} = require("express-validator");
-router.post("/", [
+router.post("/createuser", [
     body('name', "Enter valid name: ").isLength({ min:5}),
     body('email', "Enter valid email: ").isEmail(),
     body('password', "Password must be atleast 5 chars").isLength({ min: 5}),
-], (req, res)=>{
+], async (req, res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    let user = await User.findOne({email: req.body.email});
+    try{
+      if (user) {
+        return res.status(400).json({error: "Sorry a user with this email already exist"})
+    }
+    const salt = await bcrypt.genSaltSync(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    user = await User.create({
         name: req.body.name,
+        password: secPass,
         email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err =>{
-          console.log(err);
-          res.json({error: "Please enter the unique value of email", msg:err.message})
       })
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({authtoken});
+    }
+    catch(error){
+      console.error(error);
+      res.status(500).send("Some error occured");
+    }
+    //   .then(user => res.json(user))
+    //   .catch(err =>{
+    //   res.json({error: "Please enter the unique value of email", msg:err.message})})
 })
 module.exports = router;
